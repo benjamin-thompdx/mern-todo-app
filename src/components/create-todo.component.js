79d0c8e631b689c846e72dea13b07d5aa5c55b8c
Benjamin Thom <jaminthom@gmail.com>
2020-05-15 14:31:21 -0700

    update src/components/create-todo.component.js to include a button for submitting our form and updating state for our various state slices

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -110,6 +110,9 @@ export default class CreateTodo extends Component {
               <label className='form-check-label'>High</label>
             </div>
           </div>
+          <div className='form-group'>
+            <input type='submit' value='Create Todo' className='btn btn-secondary' />
+          </div>
         </form>
       </div>
     )
