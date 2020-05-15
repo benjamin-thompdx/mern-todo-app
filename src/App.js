2d94929eaaea426f20f96ab2c2850284687a7f51
Benjamin Thom <jaminthom@gmail.com>
2020-05-15 11:46:42 -0700

    update syntax within src/App.js to use className 'navbar-collapse'

--- src/App.js
+++ src/App.js
@@ -15,21 +15,20 @@ class App extends Component {
       <div className='container'>
         
         <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-          <a className='navbar-brand' href='https://benjaminthompdx.wordpress.com/' target='blank' >
-            <img src={logo} width='30' height='30' alt='benjaminthompdx.wordpress.com' />
-            <Link to='/' className='navbar-brand'>MERN-Stack Todo App</Link>
-            <div className='collapse nav-collapse'>
-              <ul className='navbar-nav mr-auto'>
-                <li className='navbar-item'>
-                  <Link to='/' className='nav-link'>Todos</Link>
-                </li>
-                <li className='navbar-item'>
-                  <Link to='/create' className='nav-link'>Create Todo</Link>
-                </li>
-              </ul>
-
-            </div>
+          <a className='navbar-brand' href='https://benjaminthompdx.wordpress.com/' target='blank'>
+            <img src={logo} width='80' height='80' alt='benjaminthompdx.wordpress.com' />
           </a>
+          <Link to='/' className='navbar-brand'>MERN-Stack Todo App</Link>
+          <div className='collapse navbar-collapse'>
+            <ul className='navbar-nav mr-auto'>
+              <li className='navbar-item'>
+                <Link to='/' className='nav-link'>Todos</Link>
+              </li>
+              <li className='navbar-item'>
+                <Link to='/create' className='nav-link'>Create Todo</Link>
+              </li>
+            </ul>
+          </div>
         </nav>
 
 
