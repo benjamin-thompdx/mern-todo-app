7b5ab67bc10c38fa95fd2a8c55926172861f5947
Benjamin Thom <jaminthom@gmail.com>
2020-05-15 13:34:21 -0700

    update src/components/create-todo.component.js to include an onSubmit method to update our state after each submission. add this.onChangeTodoDescription, this.onChangeTodoResponsible, this.onChangeTodoPriority, and this.onSubmit to access state in each of our methods

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -5,6 +5,11 @@ export default class CreateTodo extends Component {
   constructor(props) {
     super(props);
 
+    this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+    this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this)
+    this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
     this.state = {
       todo_description: '',
       todo_responsible: '',
@@ -29,6 +34,24 @@ export default class CreateTodo extends Component {
     this.setState({
       todo_priority: e.target.value
     });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    console.log(`Form submited:`);
+    console.log(`Todo Description: ${this.state.todo_description}`);
+    console.log(`Todo Responsible: ${this.state.todo_responsible}`);
+    console.log(`Todo Priority: ${this.state.todo_priority}`);
+    console.log(`Todo Completed: ${this.state.todo_completed}`);
+    //onSubmit logic will go here
+
+    this.setState({
+      todo_description: '',
+      todo_responsible: '',
+      todo_priority: '',
+      todo_completed: false
+    })
   }
 
   render() {
