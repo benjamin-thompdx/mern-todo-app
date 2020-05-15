08b7dcfa2b6f20b8f97ef5447a11bfefcd59cfa3
Benjamin Thom <jaminthom@gmail.com>
2020-05-15 10:44:36 -0700

    update src/App.js to include Routes within the div container

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ class App extends Component {
     <Router>
       <div className='container'>
         <h2>MERN-Stack Todo App</h2>
+        <Route path='/' exact component={TodosList} />
+        <Route path='/edit/:id' component={EditTodo} />
+        <Route path='/create' component={CreateTodo} />
       </div> 
-      <Route path='/' exact component={TodosList} />
-      <Route path='/edit/:id' component={EditTodo} />
-      <Route path='/create' component={CreateTodo} />
     </Router>
     );
   }
